a25f5ad550331a502e6c9249570c0a59f8e0702a
eggfly <lihaohua90@gmail.com>
2021-12-27 20:28:31 +0800

    Add disableLeakVM settings

--- android/app/src/main/kotlin/com/example/multiple_flutters/App.kt
+++ android/app/src/main/kotlin/com/example/multiple_flutters/App.kt
@@ -1,7 +1,9 @@
 package com.example.multiple_flutters
 
 import android.app.Application
+import io.flutter.FlutterInjector
 import io.flutter.embedding.engine.FlutterEngineGroup
+import io.flutter.embedding.engine.loader.FlutterLoader
 
 /**
  * Application class for this app.
@@ -13,6 +15,9 @@ class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        val settings = FlutterLoader.Settings()
+        settings.disableLeakVM()
+        FlutterInjector.instance().flutterLoader().startInitialization(applicationContext, settings)
         engines = FlutterEngineGroup(this)
     }
 }
